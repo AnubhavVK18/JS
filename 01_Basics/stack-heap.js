8df02f667df86deb3731852890f5a8f958626161
AnubhavVK18 <127076151+AnubhavVK18@users.noreply.github.com>
2024-09-03 21:09:29 +0000

    Memory

---
+++ 01_Basics/stack-heap.js
@@ -0,0 +1,17 @@
+let myYtname="anubhavchoudharydotcom"
+let anothername=myYtname
+    anothername = "chaiaurcode"
+
+    console.log(myYtname);
+    console.log(anothername);
+
+    let userOne ={
+        email:"nfawurhw@google.com",
+        upi:"user1@ybl",
+    }
+
+    let userTwo = userOne 
+        userTwo.email="anubhav@google.com"
+
+            console.log(userOne.email);
+            console.log(userTwo.email);
