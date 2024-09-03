5adc635e37d999251907fca5ecaebfa5729a7098
AnubhavVK18 <127076151+AnubhavVK18@users.noreply.github.com>
2024-09-03 14:58:25 +0000

    Conversion

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity="Jaipur"
+let accountState;
+
+// accountId = 2
+// console.log(accountId);
+accountEmail = "hkg@google.com"
+accountPassword ="12434"
+accountCity = "Bengaluru"
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
