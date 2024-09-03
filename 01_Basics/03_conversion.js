20069abebaa591cd41d7ef0e5a3516a1e94a5602
AnubhavVK18 <127076151+AnubhavVK18@users.noreply.github.com>
2024-09-03 15:38:16 +0000

    Conversion

--- 01_Basics/03_conversion.js
+++ 01_Basics/03_conversion.js
@@ -1,7 +1,35 @@
-let score = undefined
-console.log(typeof score);
-console.log(typeof (score));
+// let score = undefined
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// ***********Operations************
+
+let value =3
+let negValue = -value
+console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log(+true);
+// console.log(+"");
+
+// let gameCounter = 100
+// gameCounter++;
+// console.log(gameCounter);
+
+num1 = num2 = num3 = 2 + 2
+console.log(num1);
+console.log(num2);
