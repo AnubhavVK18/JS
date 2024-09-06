08b7a08476e89ff220309726deeb6270ddc092e1
AnubhavVK18 <127076151+AnubhavVK18@users.noreply.github.com>
2024-09-06 15:26:50 +0000

    String basic methods

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,8 @@
+const gameName=new String('hitesh-hc')
+    const newString=gameName.substring(0,4)
+        console.log(newString);
+
+        const anotherString = gameName.slice(-8,4)
+            console.log(anotherString);
+
+            console.log(gameName.split('-'));
